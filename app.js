eafb5251f69bcd2bec63b6896b92a5e8dab9df2a
cameronkloot <cameron@0ptimus.com>
2019-02-22 09:09:18 -0500

    Remove test command

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-enableProdMode()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
